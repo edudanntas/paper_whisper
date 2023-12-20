3098beeaa177fb65db6763429c2658ee8b80f7b7
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-20 09:29:04 -0300

    feat: configration of database on prisma

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,9 +1,26 @@
 import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../(trpc)/client"
+import { router } from "@/trpc/trpc"
 
 const Page = () => {
     const route = useRouter()
     const searchParams = useSearchParams()
     const origin = searchParams.get('origin')
+
+    const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({ success }) => {
+            if (success) {
+                route.push(origin ? `/${origin}` : '/dashboard')
+            }
+        },
+        onError: (err) => {
+            if (err.data?.code === "UNAUTHORIZED") {
+                route.push('/sign-in')
+            }
+        },
+        retry: true,
+        retryDelay: 500,
+    })
 }
 
 export default Page
