c586e1365f23c09f16ee56799548e08c0f57a932
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-20 17:43:39 -0300

    feat: create an url type to RenderPDF component

--- src/app/(dashboard)/dashboard/[fileid]/page.tsx
+++ src/app/(dashboard)/dashboard/[fileid]/page.tsx
@@ -33,7 +33,7 @@ const page = async ({ params }: Props) => {
                 {/* Lado esquerdo: PDF */}
                 <div className='flex-1 lg:flex'>
                     <div className="px-4 py-6 sm:px-6 lg:pl-6 xl:flex-1 xl:pl-6">
-                        <RenderPDF />
+                        <RenderPDF url={file.url} />
                     </div>
                 </div>
 
