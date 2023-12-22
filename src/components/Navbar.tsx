78200ea93d79dc25360087eee6298a030e9aa665
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-22 12:40:23 -0300

    feat: add Dashboard, Manage Subscription and logout to avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { LoginLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from "lucide-react"
+import UserAccountMenu from "./UserAccountMenu"
 
 const Navbar = () => {
+
+    const { getUser } = getKindeServerSession()
+    const user = getUser()
+
     return (
         <nav className="h-14 sticky inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -15,7 +20,7 @@ const Navbar = () => {
                     {/* NÃO ESQUECER DE FAZER A NAVBAR DO MOBILE */}
 
                     <div className="hidden items-center space-x-4 sm:flex">
-                        <>
+                        {!user ? <>
                             <Link href='/planos'
                                 className={buttonVariants({
                                     variant: 'ghost',
@@ -29,7 +34,22 @@ const Navbar = () => {
                             <RegisterLink className={buttonVariants({
                                 size: 'sm'
                             })} >Começar <ArrowRight className="ml-1.5 h-5 w-5" /> </RegisterLink>
-                        </>
+                        </> : <>
+                            <Link href='/dashboard'
+                                className={buttonVariants({
+                                    variant: 'ghost',
+                                    size: 'sm'
+                                })}
+                            >Dashboard
+                            </Link>
+                            <UserAccountMenu
+                                name={
+                                    !user.given_name || !user.family_name ? "Sua Conta" : `${user.given_name} ${user.family_name}`
+                                }
+                                email={user.email ?? ''}
+                                imageUrl={user.picture ?? ''}
+                            />
+                        </>}
                     </div>
                 </div>
             </MaxWidthWrapper>
--- src/components/UserAccountMenu.tsx
+++ src/components/UserAccountMenu.tsx
@@ -1,10 +1,13 @@
 import { getUserSubscriptionPlan } from '@/lib/stripe'
 import React from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { Button } from './ui/button'
 import { Avatar, AvatarFallback } from './ui/avatar'
 import Image from 'next/image'
 import { Icons } from './Icons'
+import Link from 'next/link'
+import { Gem } from 'lucide-react'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server'
 
 type Props = {
     email: string | undefined
@@ -31,6 +34,38 @@ const UserAccountMenu = async ({ email, imageUrl, name }: Props) => {
                     </Avatar>
                 </Button>
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent className='bg-white' align='end'>
+                <div className='flex items-center gap-2 justify-start p-2'>
+                    <div className='flex flex-col space-y-0.5 leading-none'>
+                        {name && <p className='font-medium text-sm text-black'>{name}</p>}
+                        {email && (
+                            <p className='w-[200px] truncate text-xs text-zinc-700'>{email}</p>
+                        )}
+                    </div>
+                </div>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem asChild>
+                    <Link className='cursor-pointer' href='/dashboard'>
+                        Dashboard
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                    {subscriptionPlan?.isSubscribed ? (
+                        <Link className='cursor-pointer' href='/dashboard/billing'>Gerenciar sua assinatura</Link>
+                    ) : (
+                        <Link className='cursor-pointer' href='/planos'>Assinar <Gem className='ml-1.5 h-4 w-4 text-green-500' /></Link>
+                    )}
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className='cursor-pointer' asChild>
+                    <LogoutLink>Sair</LogoutLink>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+
         </DropdownMenu>
     )
 }
