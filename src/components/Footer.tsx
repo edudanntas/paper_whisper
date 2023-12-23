09dbe1a4f4b7903f738c5b8817b11bea75f973ad
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-23 11:19:50 -0300

    style: update link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className="flex lg:h-20 items-center justify-between">
                     <span className='flex font-semibold z-40'>2023 Paper Whisper - Todos os direitos reservados</span>
                     <div className="items-center space-x-4 sm:flex">
-                        <span>Feito por <a className='font-semibold text-green-600 hover:text-green-700' href='https://github.com/edudanntas'>Eduardo Dantas</a></span>
+                        <span>Feito por <a className='font-semibold text-green-600 hover:text-green-700' href='https://www.linkedin.com/in/eduardo-danttas/'>Eduardo Dantas</a></span>
                     </div>
                 </div>
             </MaxWidthWrapper>
